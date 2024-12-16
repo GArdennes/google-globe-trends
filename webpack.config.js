2357900d45e0d2ef952843db1644049a0ce9fc76
GArdennes <thekevin.afachao@gmail.com>
2024-12-16 13:59:01 +0200

    feat: add Webpack configuration and update dependencies

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+import path from "path";
+
+export default {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "style-loader", "css-loader"],
+      },
+    ],
+  },
+};
