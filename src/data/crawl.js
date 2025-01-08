eb36a191c8805a21b742e5f6a093eefed5387458
GArdennes <thekevin.afachao@gmail.com>
2025-01-08 19:05:10 +0200

    feat: add JSON data files for various countries with goal ratings and scores

--- src/data/crawl.js
+++ src/data/crawl.js
@@ -1,101 +1,104 @@
 import fs from "fs";
-import axios from "axios";
-import { parseStringPromise } from "xml2js";
-
-function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-async function fetchWithRetry(url, retries = 3, backoff = 1000) {
-  for (let i = 0; i < retries; i++) {
-    try {
-      const response = await axios.get(url);
-      return response;
-    } catch (error) {
-      if (error.response && error.response.status === 429 && i < retries - 1) {
-        console.warn(`Rate limit exceeded. Retrying in ${backoff}ms...`);
-        await delay(backoff);
-        backoff *= 2; // Exponential backoff
-      } else {
-        throw error;
-      }
-    }
-  }
-}
+const sdg_data = JSON.parse(fs.readFileSync("./src/data/SDG.json", "utf8"));
 
 export default async function crawl(data) {
   let tableData = [];
+  let fetchPromises = [];
+
   for (const country of data) {
     try {
-      console.log(`Crawling ${country.name} with ${country["alpha-2"]}...`);
+      console.log(`Crawling ${country.name} with ${country["alpha-3"]}...`);
       let countryName = country.name;
-      let geoCode = country["alpha-2"];
-      const url = `https://trends.google.com/trending/rss?geo=${geoCode}`;
-      const response = await fetchWithRetry(url);
-      const feed = await parseStringPromise(response.data);
+      let geoCode = country["alpha-3"];
+      const feed = sdg_data.features;
+      const item = feed.find((item) => item.attributes.ID === geoCode);
 
-      const trend = feed.rss.channel[0].item.map((item) => ({
-        title: item.title[0],
-        link: item.link[0],
-        pubdate: item.pubDate[0],
-        approx_traffic: item["ht:approx_traffic"]
-          ? parseInt(item["ht:approx_traffic"][0].replace(/\D+/g, ""), 10) || 0
-          : 0,
-        picture: item["ht:picture"] ? item["ht:picture"][0] : "N/A",
-        picture_source: item["ht:picture_source"]
-          ? item["ht:picture_source"][0]
-          : "N/A",
-        news_items: item["ht:news_item"]
-          ? item["ht:news_item"].map((newsItem) => ({
-              title: newsItem["ht:news_item_title"]
-                ? newsItem["ht:news_item_title"][0]
-                : "N/A",
-              snippet: newsItem["ht:news_item_snippet"]
-                ? newsItem["ht:news_item_snippet"][0]
-                : "N/A",
-              url: newsItem["ht:news_item_url"]
-                ? newsItem["ht:news_item_url"][0]
-                : "N/A",
-              picture: newsItem["ht:news_item_picture"]
-                ? newsItem["ht:news_item_picture"][0]
-                : "N/A",
-              source: newsItem["ht:news_item_source"]
-                ? newsItem["ht:news_item_source"][0]
-                : "N/A",
-            }))
-          : [],
-      }));
+      if (item) {
+        const attributes = item.attributes;
+        const performance = {
+          "Goal 1 Rating": attributes["Goal_1_Rating"],
+          "Goal 2 Rating": attributes["Goal_2_Rating"],
+          "Goal 3 Rating": attributes["Goal_3_Rating"],
+          "Goal 4 Rating": attributes["Goal_4_Rating"],
+          "Goal 5 Rating": attributes["Goal_5_Rating"],
+          "Goal 6 Rating": attributes["Goal_6_Rating"],
+          "Goal 7 Rating": attributes["Goal_7_Rating"],
+          "Goal 8 Rating": attributes["Goal_8_Rating"],
+          "Goal 9 Rating": attributes["Goal_9_Rating"],
+          "Goal 10 Rating": attributes["Goal_10_Rating"],
+          "Goal 11 Rating": attributes["Goal_11_Rating"],
+          "Goal 12 Rating": attributes["Goal_12_Rating"],
+          "Goal 13 Rating": attributes["Goal_13_Rating"],
+          "Goal 14 Rating": attributes["Goal_14_Rating"],
+          "Goal 15 Rating": attributes["Goal_15_Rating"],
+          "Goal 16 Rating": attributes["Goal_16_Rating"],
+          "Goal 17 Rating": attributes["Goal_17_Rating"],
+          "Goal 1 Score": attributes["Goal_1_Score"],
+          "Goal 2 Score": attributes["Goal_2_Score"],
+          "Goal 3 Score": attributes["Goal_3_Score"],
+          "Goal 4 Score": attributes["Goal_4_Score"],
+          "Goal 5 Score": attributes["Goal_5_Score"],
+          "Goal 6 Score": attributes["Goal_6_Score"],
+          "Goal 7 Score": attributes["Goal_7_Score"],
+          "Goal 8 Score": attributes["Goal_8_Score"],
+          "Goal 9 Score": attributes["Goal_9_Score"],
+          "Goal 10 Score": attributes["Goal_10_Score"],
+          "Goal 11 Score": attributes["Goal_11_Score"],
+          "Goal 12 Score": attributes["Goal_12_Score"],
+          "Goal 13 Score": attributes["Goal_13_Score"],
+          "Goal 14 Score": attributes["Goal_14_Score"],
+          "Goal 15 Score": attributes["Goal_15_Score"],
+          "Goal 16 Score": attributes["Goal_16_Score"],
+          "Goal 17 Score": attributes["Goal_17_Score"],
+        };
 
-      const maxTrend = trend.reduce(
-        (max, item) => (item.approx_traffic > max.approx_traffic ? item : max),
-        trend[0],
-      );
+        const fetchPromise = fetch("https://restcountries.com/v3.1/alpha/" + geoCode)
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          })
+          .then((point) => {
+            console.log(JSON.stringify(point, null, 2)); // Log the entire point object to inspect its structure
+            const x = {
+              country: countryName,
+              ISO: geoCode,
+              population: Array.isArray(point) && point.length > 0 ? point[0].population : null,
+              performance,
+            };
 
-      const x = {
-        country: countryName,
-        ISO: geoCode,
-        title: maxTrend.title,
-        link: maxTrend.news_items[0] ? maxTrend.news_items[0].url : "N/A",
-        traffic: maxTrend.approx_traffic,
-      };
+            tableData.push(x);
+            fs.writeFileSync(
+              `./src/data/countries/${countryName}.json`,
+              JSON.stringify(performance, null, 2),
+            );
+          })
+          .catch((error) => {
+            console.error(
+              "There has been a problem with your fetch operation:",
+              error,
+            );
+          });
 
-      tableData.push(x);
-      fs.writeFileSync(
-        `./src/data/countries/${countryName}.json`,
-        JSON.stringify(trend, null, 2),
-      );
+        fetchPromises.push(fetchPromise);
+      } else {
+        console.log(`No item found for ${countryName} with code ${geoCode}`);
+      }
     } catch (error) {
       console.error(`Error crawling ${country.name}:`, error.message);
       continue;
     }
-    await delay(1000); // Delay for 1 second between requests
   }
 
-  const res = {
-    lastUpdate: new Date().toISOString(),
-    data: tableData,
-  };
-  fs.writeFileSync(`./src/data/data.json`, JSON.stringify(res, null, 2));
+  // Wait for all fetch calls to complete
+  await Promise.all(fetchPromises);
+
+  // Log tableData to ensure it is populated
+  // console.log("Final tableData:", JSON.stringify(tableData, null, 2));
+
+  // Write tableData to data.json
+  fs.writeFileSync(`./src/data/data.json`, JSON.stringify(tableData, null, 2));
   // fs.writeFileSync(`./public/data/data.json`, JSON.stringify(res, null, 2));
   return "Crawl completed";
 }
@@ -104,8 +107,3 @@ var list = JSON.parse(fs.readFileSync("./src/data/all.json", "utf8"));
 (async () => {
   await crawl(list);
 })();
-
-// const url = `https://trends.google.com/trending/rss?geo=ZA`;
-// const feed = await axios.get(url);
-// const response = await parser.parseStringPromise(feed.data);
-// console.log(response.rss.channel[0].item);
