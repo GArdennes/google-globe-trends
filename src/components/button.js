e7e48698e7f6eec24e83bfead88efed6e2832577
Kevin Afachao <thekevin.afachao@gmail.com>
2025-10-21 17:12:27 +0000

    refactor: remove unused components and clean up the codebase

--- src/components/button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Button({ label, onClick }) {
-  return (
-    <button className="button" onClick={onClick}>
-      {label}
-    </button>
-  );
-}
--- src/components/description.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Description() {
-  return <>Visualizing Google Trends data on a 3D globe.</>;
-}
